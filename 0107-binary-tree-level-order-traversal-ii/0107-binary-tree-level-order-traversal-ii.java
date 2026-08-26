d3d5829d78c24eb788385b661e3334cca1f0e1c3
TJ Sadiya khanem <229x1a2920@gmail.com>
2026-08-26 18:25:15 +0530

    Time: 1 ms (97.69%), Space: 44.4 MB (76.88%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+       
+
+        while (!queue.isEmpty()) {
+
+            int levelsize = queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            while (levelsize > 0) {
+
+
+                TreeNode temp = queue.poll();
+
+                list.add(temp.val);
+               
+                 if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+               
+
+                levelsize--;
+            }
+
+            ans.add(list);
+        }
+        Collections.reverse(ans);
+
+        return ans;
+
+        
+    }
+}
